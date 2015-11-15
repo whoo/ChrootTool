79c45c38f4a7817a80f85acf1ac599830ceecc5f
Dominique DERRIER <derrierdo@gmail.com>
2015-11-15 16:16:59 -0500

    Init

---
+++ Chrootbin.sh
@@ -0,0 +1,42 @@
+#!/bin/bash 
+
+
+function green { echo "[32m$1[00m"; }
+function red { echo "[31m$1[00m"; }
+
+
+function push
+{
+dir=$(dirname "$1")
+if [ ! -d $dir ] 
+then 
+#red "create $dir"
+mkdir -p "$dir" 
+fi
+
+cp "/$1" "$dir"
+
+}
+
+##   Essential ###
+push lib64/ld-linux-x86-64.so.2
+push lib32/ld-linux.so.2
+mkdir bin sbin
+###############################
+
+for a in "$@"
+do
+
+for b in $(LD_TRACE_LOADED_OBJECTS=1 $a | awk '/=>/{print $3}')
+do
+	### Push all lib
+	green "${b}"
+	push "${b#\/}"
+done
+
+### Push bin
+red "${a}"
+push "${a#\/}" 
+
+done
+
---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM scratch
+MAINTAINER Dominique DERRIER <derrierdo@gmail.com>
+COPY . /
